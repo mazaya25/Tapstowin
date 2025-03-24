e588de1b7836bae16a3ebc9c42f6ddec914a8b4b
mazaya25 <74729788+mazaya25@users.noreply.github.com>
2025-03-24 01:09:38 +0100

    Update start.js

--- API/start.js
+++ API/start.js
@@ -1,4 +1,3 @@
-
 export default function handler(req, res) {
     if (req.method === 'POST') {
         // Warpcast'ten gelen POST isteğini işleme
@@ -15,7 +14,7 @@ export default function handler(req, res) {
                 <meta property="fc:frame:image" content="https://cointaps.xyz/preview.png">
                 <meta property="fc:frame:button:1" content="Play Now">
                 <meta property="fc:frame:button:1:action" content="link">
-                <meta property="fc:frame:button:1:target" content="https://cointaps.xyz">
+                <meta property="fc:frame:button:1:target" content="https://cointaps.xyz/index.html">
                 <title>Cointaps Game Started</title>
             </head>
             <body>
